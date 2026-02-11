783a906ac11b4ffe78a3cd9a9d6ed2ee10df6daf
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-02-11 17:59:33 +0100

    cleaning up

--- Mathlib/CategoryTheory/Functor/ReflectsIso/Jointly.lean
+++ Mathlib/CategoryTheory/Functor/ReflectsIso/Jointly.lean
@@ -43,30 +43,32 @@ such that the image of `f` under all `F i` is an epimorphism, then `f` is an epi
 structure JointlyReflectEpimorphisms (F : ∀ i, C ⥤ D i) : Prop where
   epi {X Y : C} (f : X ⟶ Y) [∀ i, Epi ((F i).map f)] : Epi f
 
+variable {F}
+
 namespace JointlyReflectIsomorphisms
 
-variable {F} (h : JointlyReflectIsomorphisms F)
+variable (h : JointlyReflectIsomorphisms F)
 
-include h in
-lemma isIso_iff {X Y : C} (f : X ⟶ Y) : IsIso f ↔ ∀ i, IsIso ((F i).map f) :=
+include h
+
+lemma isIso_iff {X Y : C} (f : X ⟶ Y) :
+    IsIso f ↔ ∀ i, IsIso ((F i).map f) :=
   ⟨fun _ _ ↦ inferInstance, fun _ ↦ h.isIso f⟩
 
-include h in
 lemma mono {X Y : C} (f : X ⟶ Y) [hf : ∀ i, Mono ((F i).map f)]
-    [∀ i, PreservesLimit (cospan f f) (F i)] [HasPullback f f] : Mono f := by
+    [∀ i, PreservesLimit (cospan f f) (F i)] [HasPullback f f] :
+    Mono f := by
   have hc := pullbackIsPullback f f
   rw [mono_iff_isIso_fst hc, h.isIso_iff]
   intro i
   exact (mono_iff_isIso_fst ((isLimitMapConePullbackConeEquiv (F i) pullback.condition).1
     (isLimitOfPreserves (F i) hc))).1 (hf i)
 
-include h in
 lemma jointlyReflectMonomorphisms [∀ i, PreservesLimitsOfShape WalkingCospan (F i)]
     [HasPullbacks C] :
     JointlyReflectMonomorphisms F where
   mono f _ := h.mono f
 
-include h in
 lemma epi {X Y : C} (f : X ⟶ Y) [hf : ∀ i, Epi ((F i).map f)]
     [∀ i, PreservesColimit (span f f) (F i)] [HasPushout f f] : Epi f := by
   have hc := pushoutIsPushout f f
@@ -75,7 +77,6 @@ lemma epi {X Y : C} (f : X ⟶ Y) [hf : ∀ i, Epi ((F i).map f)]
   exact (epi_iff_isIso_inl ((isColimitMapCoconePushoutCoconeEquiv (F i) pushout.condition).1
     (isColimitOfPreserves (F i) hc))).1 (hf i)
 
-include h in
 lemma jointlyReflectEpimorphisms [∀ i, PreservesColimitsOfShape WalkingSpan (F i)]
     [HasPushouts C] :
     JointlyReflectEpimorphisms F where
@@ -83,26 +84,14 @@ lemma jointlyReflectEpimorphisms [∀ i, PreservesColimitsOfShape WalkingSpan (F
 
 end JointlyReflectIsomorphisms
 
-namespace JointlyReflectMonomorphisms
-
-variable {F} (h : JointlyReflectMonomorphisms F)
-
-include h in
-lemma mono_iff [∀ i, (F i).PreservesMonomorphisms] {X Y : C} (f : X ⟶ Y) :
+lemma JointlyReflectMonomorphisms.mono_iff (h : JointlyReflectMonomorphisms F)
+    [∀ i, (F i).PreservesMonomorphisms] {X Y : C} (f : X ⟶ Y) :
     Mono f ↔ ∀ i, Mono ((F i).map f) :=
   ⟨fun _ _ ↦ inferInstance, fun _ ↦ h.mono f⟩
 
-end JointlyReflectMonomorphisms
-
-namespace JointlyReflectEpimorphisms
-
-variable {F} (h : JointlyReflectEpimorphisms F)
-
-include h in
-lemma epi_iff [∀ i, (F i).PreservesEpimorphisms] {X Y : C} (f : X ⟶ Y) :
+lemma JointlyReflectEpimorphisms.epi_iff (h : JointlyReflectEpimorphisms F)
+    [∀ i, (F i).PreservesEpimorphisms] {X Y : C} (f : X ⟶ Y) :
     Epi f ↔ ∀ i, Epi ((F i).map f) :=
   ⟨fun _ _ ↦ inferInstance, fun _ ↦ h.epi f⟩
-
-end JointlyReflectEpimorphisms
 
 end CategoryTheory
