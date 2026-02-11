8407a7143bed1bbb149d8a1a94ea8b555de177d3
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-02-11 17:46:46 +0100

    feat(CategoryTheory/Limits): terminal object in functor categories

---
+++ Mathlib/CategoryTheory/Limits/FunctorCategory/Shapes/Terminal.lean
@@ -0,0 +1,51 @@
+/-
+Copyright (c) 2026 Joël Riou. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joël Riou
+-/
+module
+
+public import Mathlib.CategoryTheory.Limits.FunctorCategory.Basic
+public import Mathlib.CategoryTheory.Limits.Shapes.Terminal
+
+/-!
+# Initial and terminal objects in the category of functors
+
+We show that if a functor `F : C ⥤ D` is such that `F.obj X`
+is terminal for all `X`, then `F` is a terminal object.
+
+-/
+
+@[expose] public section
+
+namespace CategoryTheory.Functor
+
+open Limits
+
+variable {C D : Type*} [Category* C] [Category D]
+
+/-- If `F : C ⥤ D` is such that `F.obj X` is terminal for any `X : C`,
+then `F` is a terminal object. -/
+def isTerminal {F : C ⥤ D} (hF : ∀ (X : C), IsTerminal (F.obj X)) :
+    IsTerminal F :=
+  IsTerminal.ofUniqueHom
+    (fun G ↦
+      { app _ := (hF _).from _
+        naturality _ _ _ := (hF _).hom_ext _ _ })
+    (fun _ _ ↦ by
+      ext
+      apply (hF _).hom_ext )
+
+/-- If `F : C ⥤ D` is such that `F.obj X` is initial for any `X : C`,
+then `F` is an initial object. -/
+def isInitial {F : C ⥤ D} (hF : ∀ (X : C), IsInitial (F.obj X)) :
+    IsInitial F :=
+  IsInitial.ofUniqueHom
+    (fun G ↦
+      { app _ := (hF _).to _
+        naturality _ _ _ := (hF _).hom_ext _ _ })
+    (fun _ _ ↦ by
+      ext
+      apply (hF _).hom_ext )
+
+end CategoryTheory.Functor
