fe985591420e8f1f2c02eec3f1668cf9c022e48b
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-02-15 17:14:37 +0100

    trunc-legt

--- Mathlib.lean
+++ Mathlib.lean
@@ -3235,6 +3235,7 @@ public import Mathlib.CategoryTheory.Triangulated.Rotate
 public import Mathlib.CategoryTheory.Triangulated.SpectralObject
 public import Mathlib.CategoryTheory.Triangulated.Subcategory
 public import Mathlib.CategoryTheory.Triangulated.TStructure.Basic
+public import Mathlib.CategoryTheory.Triangulated.TStructure.TruncLEGT
 public import Mathlib.CategoryTheory.Triangulated.TStructure.TruncLTGE
 public import Mathlib.CategoryTheory.Triangulated.TriangleShift
 public import Mathlib.CategoryTheory.Triangulated.Triangulated
