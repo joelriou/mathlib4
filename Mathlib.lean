714915b2242c685fcd6ec3b887cc164ff15305f2
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-02-16 11:29:18 +0100

    feat(CategoryTheory/Sites): descent data when we have pullbacks

--- Mathlib.lean
+++ Mathlib.lean
@@ -3120,6 +3120,7 @@ public import Mathlib.CategoryTheory.Sites.DenseSubsite.InducedTopology
 public import Mathlib.CategoryTheory.Sites.DenseSubsite.OneHypercoverDense
 public import Mathlib.CategoryTheory.Sites.DenseSubsite.SheafEquiv
 public import Mathlib.CategoryTheory.Sites.Descent.DescentData
+public import Mathlib.CategoryTheory.Sites.Descent.DescentDataPrime
 public import Mathlib.CategoryTheory.Sites.Descent.IsPrestack
 public import Mathlib.CategoryTheory.Sites.Descent.IsStack
 public import Mathlib.CategoryTheory.Sites.Descent.Precoverage
