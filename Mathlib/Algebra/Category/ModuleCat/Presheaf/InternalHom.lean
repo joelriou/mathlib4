b20128e4d894d09399824efe1f406688c010d9b1
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-02-19 21:30:29 +0100

    wip

--- Mathlib/Algebra/Category/ModuleCat/Presheaf/InternalHom.lean
+++ Mathlib/Algebra/Category/ModuleCat/Presheaf/InternalHom.lean
@@ -101,18 +101,22 @@ abbrev InternalHomObj (U : C) : Type _ := F.over U ⟶ G.over U
 @[simps]
 noncomputable def internalHomMap {U V : C} (f : V ⟶ U) (φ : InternalHomObj F G U) :
     InternalHomObj F G V where
-  app W := ModuleCat.ofHom
-    { toFun s := φ.app ((Over.map f).op.obj W) s
-      map_add' := sorry
-      map_smul' := sorry }
-  naturality := sorry
+  app W := φ.app ((Over.map f).op.obj W)
+  naturality g := φ.naturality ((Over.map f).op.map g)
 
 noncomputable def internalHom : PresheafOfModules.{max u u₁ v₁} (R ⋙ forget₂ _ _) where
   obj U := ModuleCat.of (R.obj U) (InternalHomObj F G U.unop)
   map {U V} f := ConcreteCategory.ofHom (C := ModuleCat (R.obj U))
     { toFun := internalHomMap _ _ f.unop
-      map_add' := sorry
-      map_smul' := sorry }
+      map_add' _ _ := rfl
+      map_smul' _ _ := PresheafOfModules.hom_ext (fun _ ↦ by
+        ext
+        dsimp [internalHomMap]
+        erw [over_smul_app_apply, over_smul_app_apply]
+        dsimp
+        rw [Functor.map_comp]
+        rfl
+        ) }
   map_id _ := by
     ext
     dsimp [internalHomMap]
@@ -123,11 +127,12 @@ noncomputable def internalHom : PresheafOfModules.{max u u₁ v₁} (R ⋙ forge
     rw [ModuleCat.restrictScalarsId'App_inv_apply]
     congr
     simp [Over.mapId_eq]
-  map_comp _ _ := by
+  map_comp {X₁ X₂ X₃} f g := by
     ext
     dsimp [internalHomMap]
     refine PresheafOfModules.hom_ext (fun _ ↦ ?_)
     dsimp
-    sorry
+    congr 2
+    simp [Over.mapComp_eq]
 
 end PresheafOfModules
