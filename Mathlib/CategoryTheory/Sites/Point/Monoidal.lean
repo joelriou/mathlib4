b28af40ba79e9e0f4cc5f0e666835bbe2db88edc
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-02-20 20:29:03 +0100

    wip

--- Mathlib/CategoryTheory/Sites/Point/Monoidal.lean
+++ Mathlib/CategoryTheory/Sites/Point/Monoidal.lean
@@ -12,118 +12,22 @@ public import Mathlib.CategoryTheory.Monoidal.Multifunctor
 /-!
 # Fiber functors are monoidal
 
+Let `Φ` be a point of a site `(C, J)`. Let `A` be a monoidal category where
+the tensor product commutes with filtered colimits in both variables.
+We show that the fiber functor `Φ.presheafFiber : (Cᵒᵖ ⥤ A) ⥤ A`
+is monoidal.
+
+## TODO (@joelriou)
+* for sites that have enough points, use this to give an alternative
+  proof of the assumption to `Sheaf.monoidalCategory` in the site
+  `Mathlib/CategoryTheory/Sites/Monoidal`
+* show that `Φ.sheafFiber` is also monoidal
 
 -/
 
 @[expose] public section
 
 universe w v v' u u'
-
-namespace CategoryTheory.Limits
-
-open Functor
-
-variable
-  {C₁ C₂ C : Type*} [Category* C₁] [Category* C₂] [Category* C]
-  (F : C₁ ⥤ C₂ ⥤ C)
-
-section
-
-variable (J₁ J₂ : Type*) [Category* J₁] [Category* J₂]
-  [HasColimitsOfShape J₁ C₁] [HasColimitsOfShape J₂ C₂]
-  [HasColimitsOfShape (J₁ × J₂) C]
-
-noncomputable def colim₂Comparison :
-    curry.obj (prodFunctor ⋙
-      (whiskeringRight (J₁ × J₂) (C₁ × C₂) C).obj (uncurry.obj F) ⋙ colim) ⟶
-    colim (J := J₁) ⋙ (colim (J := J₂) ⋙ F.flip).flip where
-  app G₁ :=
-    { app G₂ := colimit.desc _ (F.mapCocone₂ (colimit.cocone G₁) (colimit.cocone G₂))
-      naturality G₂ G₂' f := by
-        dsimp
-        ext j
-        have h₁ : colimit.ι (G₁.prod G₂' ⋙ uncurry.obj F) j ≫ _ = _ :=
-          colimit.ι_desc
-            (F.mapCocone₂ (colimit.cocone G₁) (colimit.cocone G₂')) _
-        have h₂ : colimit.ι (G₁.prod G₂ ⋙ uncurry.obj F) j ≫ _ = _ :=
-          colimit.ι_desc (F.mapCocone₂ (colimit.cocone G₁) (colimit.cocone G₂)) _
-        simp [h₁, reassoc_of% h₂, ← Functor.map_comp] }
-  naturality G₁ G₁' f := by
-    ext G₂
-    dsimp
-    ext j
-    have h₁ : colimit.ι (G₁'.prod G₂ ⋙ uncurry.obj F) j ≫ _ = _ :=
-      colimit.ι_desc (F.mapCocone₂ (colimit.cocone G₁') (colimit.cocone G₂)) j
-    have h₂ : colimit.ι (G₁.prod G₂ ⋙ uncurry.obj F) j ≫ _ = _ :=
-      colimit.ι_desc (F.mapCocone₂ (colimit.cocone G₁) (colimit.cocone G₂)) j
-    simp [h₁, reassoc_of% h₂, ← NatTrans.comp_app_assoc, ← Functor.map_comp]
-
-variable {J₁ J₂} in
-@[reassoc (attr := simp)]
-lemma ι_colim₂Comparison_app_app (G₁ : J₁ ⥤ C₁) (G₂ : J₂ ⥤ C₂) (j : J₁ × J₂) :
-    colimit.ι (G₁.prod G₂ ⋙ uncurry.obj F) j ≫ ((colim₂Comparison F J₁ J₂).app G₁).app G₂ =
-      (F.map (colimit.ι G₁ j.1)).app _ ≫ (F.obj _).map (colimit.ι G₂ j.2) :=
-  colimit.ι_desc _ _
-
-variable [∀ (G₁ : J₁ ⥤ C₁) (G₂ : J₂ ⥤ C₂), PreservesColimit₂ G₁ G₂ F]
-
-instance isIso_colim₂Comparison :
-    IsIso (colim₂Comparison F J₁ J₂) := by
-  simp only [NatTrans.isIso_iff_isIso_app]
-  intro G₁ G₂
-  exact (IsColimit.coconePointUniqueUpToIso (colimit.isColimit _)
-    (isColimitOfPreserves₂ F (colimit.isColimit G₁) (colimit.isColimit G₂))).isIso_hom
-
-@[simps! hom]
-noncomputable def colim₂ComparisonIso :
-    curry.obj (prodFunctor ⋙
-      (whiskeringRight (J₁ × J₂) (C₁ × C₂) C).obj (uncurry.obj F) ⋙ colim) ≅
-    colim (J := J₁) ⋙ (colim (J := J₂) ⋙ F.flip).flip :=
-  asIso (colim₂Comparison F J₁ J₂)
-
-end
-
-section
-
-variable (J : Type*) [Category* J]
-  [HasColimitsOfShape (J × J) C]
-  [HasColimitsOfShape J C]
-  (G₁ : J ⥤ C₁) (G₂ : J ⥤ C₂)
-
-noncomputable def colim₂DiagComparison :
-    curry.obj (prodFunctorToFunctorProd J C₁ C₂ ⋙ (whiskeringRight J _ C).obj (uncurry.obj F) ⋙
-      colim (J := J)) ⟶
-    curry.obj (prodFunctor ⋙
-      (whiskeringRight (J × J) (C₁ × C₂) C).obj (uncurry.obj F) ⋙ colim) where
-  app G₁ :=
-    { app G₂ := colimit.pre (E := Functor.diag J) (F := G₁.prod G₂ ⋙ uncurry.obj F)
-      naturality G₂ G₂' f := by dsimp; symm; apply colimit.pre_map }
-  naturality G₁ G₁' f := by ext; dsimp; symm; apply colimit.pre_map
-
-@[reassoc (attr := simp)]
-lemma ι_colim₂DiagComparison_app_app (G₁ : J ⥤ C₁) (G₂ : J ⥤ C₂) (j : J) :
-    colimit.ι (G₁.prod' G₂ ⋙ uncurry.obj F) j ≫ ((colim₂DiagComparison F J).app G₁).app G₂ =
-      colimit.ι (G₁.prod G₂ ⋙ uncurry.obj F) ⟨j, j⟩ :=
-  colimit.ι_desc _ _
-
-instance isIso_colim₂DiagComparison [IsSiftedOrEmpty J] :
-    IsIso (colim₂DiagComparison F J) := by
-  simp only [NatTrans.isIso_iff_isIso_app]
-  intro _ _
-  dsimp [colim₂DiagComparison]
-  infer_instance
-
-@[simps! hom]
-noncomputable def colim₂DiagComparisonIso [IsSiftedOrEmpty J] :
-    curry.obj (prodFunctorToFunctorProd J C₁ C₂ ⋙ (whiskeringRight J _ C).obj (uncurry.obj F) ⋙
-      colim (J := J)) ≅
-    curry.obj (prodFunctor ⋙
-      (whiskeringRight (J × J) (C₁ × C₂) C).obj (uncurry.obj F) ⋙ colim) :=
-  asIso (colim₂DiagComparison F J)
-
-end
-
-end CategoryTheory.Limits
 
 namespace CategoryTheory.GrothendieckTopology.Point
 
@@ -134,56 +38,48 @@ variable {C : Type u} [Category.{v} C] {J : GrothendieckTopology C}
   {A : Type u'} [Category.{v'} A] [HasColimitsOfSize.{w, w} A]
   [MonoidalCategory A]
 
-instance : HasColimitsOfShape (Φ.fiber.Elementsᵒᵖ × Φ.fiber.Elementsᵒᵖ) A :=
-  hasColimitsOfShape_of_finallySmall _ _
-
-attribute [local simp] tensorHom_def toPresheafFiber presheafFiber in
-noncomputable def δ : curriedTensorPost (Φ.presheafFiber (A := A)) ⟶
-    curriedTensorPre Φ.presheafFiber :=
-  letI α := (whiskeringLeft _ _ A).obj (CategoryOfElements.π Φ.fiber).op
-  { app G₁ := { app G₂ := colimMap { app j := by exact 𝟙 _  } } } ≫
-    (((whiskeringLeft₂ _).obj α).obj α).map
-      (colim₂DiagComparison (curriedTensor A) Φ.fiber.Elementsᵒᵖ ≫
-        colim₂Comparison _ _ _)
-
-attribute [local simp] tensorHom_def δ toPresheafFiber in
-@[reassoc (attr := simp)]
-lemma toPresheafFiber_δ_app_app (X : C) (x : Φ.fiber.obj X) (G₁ G₂ : Cᵒᵖ ⥤ A) :
-    Φ.toPresheafFiber X x (G₁ ⊗ G₂) ≫ (Φ.δ.app G₁).app G₂ =
-      Φ.toPresheafFiber X x G₁ ⊗ₘ Φ.toPresheafFiber X x G₂ := by
-  cat_disch
-
-noncomputable def η : Φ.presheafFiber.obj (𝟙_ (Cᵒᵖ ⥤ A)) ⟶ 𝟙_ A :=
-  Φ.presheafFiberDesc (fun _ _ ↦ 𝟙 _)
+noncomputable instance :
+    (Φ.presheafFiber (A := A)).OplaxMonoidal where
+  η := Φ.presheafFiberDesc (fun _ _ ↦ 𝟙 _)
+  δ P₁ P₂ :=
+    Φ.presheafFiberDesc (fun X x ↦ Φ.toPresheafFiber X x P₁ ⊗ₘ Φ.toPresheafFiber X x P₂)
+      (by simp [tensorHom_comp_tensorHom])
+  δ_natural_left _ _ := by
+    ext
+    simp [tensorHom_def', ← comp_whiskerRight, ← whisker_exchange_assoc]
+  δ_natural_right P f := by
+    ext
+    simp [tensorHom_def, ← MonoidalCategory.whiskerLeft_comp, whisker_exchange_assoc]
+  oplax_associativity P₁ P₂ P₃ := by
+    ext X x
+    dsimp
+    conv_lhs =>
+      simp only [toPresheafFiber_presheafFiberDesc_assoc, tensorHom_def'_assoc,
+        ← comp_whiskerRight_assoc, toPresheafFiber_presheafFiberDesc]
+      rw [← tensorHom_def'_assoc, associator_naturality]
+    conv_rhs =>
+      simp only [toPresheafFiber_naturality_assoc, toPresheafFiber_presheafFiberDesc_assoc,
+        tensorHom_def_assoc, ← MonoidalCategory.whiskerLeft_comp,
+        toPresheafFiber_presheafFiberDesc]
+      rw [← tensorHom_def]
+      dsimp
+  oplax_left_unitality _ := by
+    ext
+    simp [tensorHom_def', ← comp_whiskerRight]
+  oplax_right_unitality _ := by
+    ext
+    simp [tensorHom_def, ← MonoidalCategory.whiskerLeft_comp]
 
 @[reassoc (attr := simp)]
 lemma toPresheafFiber_η (X : C) (x : Φ.fiber.obj X) :
-    Φ.toPresheafFiber X x (𝟙_ (Cᵒᵖ ⥤ A)) ≫ Φ.η (A := A) = 𝟙 (𝟙_ A) := by
-  simp [η]
+    Φ.toPresheafFiber X x (𝟙_ (Cᵒᵖ ⥤ A)) ≫ OplaxMonoidal.η Φ.presheafFiber = 𝟙 (𝟙_ A) :=
+  toPresheafFiber_presheafFiberDesc _ _ _ _ _
 
-attribute [local instance] IsFiltered.isConnected in
-instance : IsIso (Φ.η (A := A)) :=
-  (IsColimit.coconePointUniqueUpToIso (colimit.isColimit _)
-    (isColimitConstCocone _ (𝟙_ A))).isIso_hom
-
-noncomputable instance : (Φ.presheafFiber (A := A)).OplaxMonoidal :=
-  .ofBifunctor Φ.η Φ.δ (by
-    ext G₁ G₂ G₃
-    refine Φ.presheafFiber_hom_ext (fun X x ↦ ?_)
-    dsimp
-    rw [toPresheafFiber_δ_app_app_assoc, tensorHom_def'_assoc,
-      ← comp_whiskerRight_assoc, toPresheafFiber_δ_app_app, ← tensorHom_def'_assoc,
-      toPresheafFiber_naturality_assoc, toPresheafFiber_δ_app_app_assoc]
-    nth_rw 2 [tensorHom_def_assoc]
-    rw [← MonoidalCategory.whiskerLeft_comp, toPresheafFiber_δ_app_app, ← tensorHom_def,
-      associator_naturality]
-    dsimp)
-    (by ext; simp [tensorHom_def', ← comp_whiskerRight])
-    (by ext; simp [tensorHom_def, ← MonoidalCategory.whiskerLeft_comp])
-
-instance :
-    IsIso (Functor.OplaxMonoidal.η (Φ.presheafFiber (A := A))) :=
-  inferInstanceAs (IsIso Φ.η)
+@[reassoc (attr := simp)]
+lemma toPresheafFiber_δ_app_app (X : C) (x : Φ.fiber.obj X) (G₁ G₂ : Cᵒᵖ ⥤ A) :
+    Φ.toPresheafFiber X x (G₁ ⊗ G₂) ≫ OplaxMonoidal.δ Φ.presheafFiber G₁ G₂ =
+      Φ.toPresheafFiber X x G₁ ⊗ₘ Φ.toPresheafFiber X x G₂ :=
+  toPresheafFiber_presheafFiberDesc _ _ _ _ _
 
 variable [LocallySmall.{w} C]
   [∀ (X : A), PreservesFilteredColimitsOfSize.{w, w} (tensorLeft X)]
@@ -197,20 +93,27 @@ instance (M : A) :
     PreservesColimitsOfShape Φ.fiber.Elementsᵒᵖ ((curriedTensor A).obj M) :=
   Functor.Final.preservesColimitsOfShape_of_final (FinallySmall.fromFilteredFinalModel.{w} _) _
 
-instance : IsIso (Φ.δ (A := A)) := by
-  simp only [NatTrans.isIso_iff_isIso_app]
-  intro G₁ G₂
-  dsimp [δ]
-  rw [isIso_comp_right_iff]
+attribute [local instance] IsFiltered.isConnected in
+instance : IsIso (OplaxMonoidal.η (Φ.presheafFiber (A := A))) :=
+  (IsColimit.coconePointUniqueUpToIso (colimit.isColimit _)
+    (isColimitConstCocone _ (𝟙_ A))).isIso_hom
+
+attribute [local simp] tensorHom_def toPresheafFiber OplaxMonoidal.δ presheafFiberDesc in
+instance (P₁ P₂ : Cᵒᵖ ⥤ A) :
+    IsIso (OplaxMonoidal.δ Φ.presheafFiber P₁ P₂) := by
+  let e := IsColimit.coconePointUniqueUpToIso (colimit.isColimit _)
+    ((Final.isColimitWhiskerEquiv (Functor.diag _) _ ).2
+      (isColimitOfPreserves₂ (curriedTensor A)
+      (colimit.isColimit ((CategoryOfElements.π Φ.fiber).op ⋙ P₁))
+      (colimit.isColimit ((CategoryOfElements.π Φ.fiber).op ⋙ P₂))))
+  rw [show OplaxMonoidal.δ Φ.presheafFiber P₁ P₂ =
+      colimMap { app _ := by exact 𝟙 _ } ≫ e.hom by cat_disch,
+    isIso_comp_right_iff]
   apply +allowSynthFailures isIso_colimMap
   rw [NatTrans.isIso_iff_isIso_app]
   intro
   dsimp
   infer_instance
-
-instance (G₁ G₂ : Cᵒᵖ ⥤ A) :
-    IsIso (Functor.OplaxMonoidal.δ (Φ.presheafFiber) G₁ G₂) :=
-  inferInstanceAs (IsIso ((Φ.δ.app G₁).app G₂))
 
 noncomputable instance : (Φ.presheafFiber (A := A)).Monoidal :=
   .ofOplaxMonoidal _
